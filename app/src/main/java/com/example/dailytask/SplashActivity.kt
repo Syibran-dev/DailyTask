a6faceb49112b0db553c87ae8398069b876415a7
afifmaker <afif99amrullah@gmail.com>
2025-11-30 01:37:24 +0700

    Feat: Final manifest configuration

--- app/src/main/java/com/example/dailytask/SplashActivity.kt
+++ app/src/main/java/com/example/dailytask/SplashActivity.kt
@@ -3,16 +3,31 @@ package com.example.dailytask
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
+import android.os.Handler
+import android.os.Looper
 
 class SplashActivity : AppCompatActivity() {
+
+    // Waktu delay sebelum pindah (3 detik)
+    private val SPLASH_TIME_OUT: Long = 3000
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
 
-        val btn = findViewById<Button>(R.id.btnMasuk)
-        btn.setOnClickListener {
-            startActivity(Intent(this, LoginActivity::class.java))
-        }
+        // Handler untuk menunda eksekusi kode
+        Handler(Looper.getMainLooper()).postDelayed({
+
+            // Pindah ke LoginActivity
+            val intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
+
+            // Tambahkan Efek Animasi Transisi (Fade)
+            overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out)
+
+            // Tutup Splash Activity
+            finish()
+
+        }, SPLASH_TIME_OUT)
     }
 }
